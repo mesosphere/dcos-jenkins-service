a84559a843a691c8a8e63101956a0cd29768231b
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2015-04-15 13:59:54 -0700

    adding build script
    Former-commit-id: ad6f37ffd6798e40169f13f15bfba1a5da2afb00

---
+++ build.sh
@@ -0,0 +1,7 @@
+rm -f jenkins-on-mesos.tgz
+mkdir jenkins-on-mesos
+cp -r *.war jenkins-on-mesos
+cp -r *.xml jenkins-on-mesos
+cp -r plugins jenkins-on-mesos
+tar czf jenkins-on-mesos.tgz jenkins-on-mesos
+rm -rf jenkins-on-mesos

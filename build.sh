1a96cf2aae3ea9a30bf77caf0cef328aadc7ea5d
Roger Ignazio <roger@mesosphere.io>
2015-10-05 17:56:28 -0700

    remove binaries and unused scripts
    Former-commit-id: 41e29db16bd6d13e62b76497e58c3f7096e737b8

--- build.sh
+++
@@ -1,8 +0,0 @@
-export COPYFILE_DISABLE=1
-rm -f jenkins-on-mesos.tgz
-mkdir jenkins-on-mesos
-cp -r *.war jenkins-on-mesos
-cp -r *.xml jenkins-on-mesos
-cp -r plugins jenkins-on-mesos
-tar czf jenkins-on-mesos.tgz jenkins-on-mesos
-rm -rf jenkins-on-mesos

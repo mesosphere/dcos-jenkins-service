a3d4ede141953773190e4b3bac9daa1d8e421826
mikestaub <mikestaub22@gmail.com>
2016-02-26 17:46:21 -0800

    use overlay storage driver instead of vfs

--- dind-agent/wrapper.sh
+++ dind-agent/wrapper.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "==> Launching the Docker daemon..."
-dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=vfs &
+dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=overlay &
 
 while(! docker info > /dev/null 2>&1); do
     echo "==> Waiting for the Docker daemon to come online..."

3508cd77f5e8a2e6eee35c28bd8cecf2a35f279f
Colin C <colin@mesosphere.com>
2016-03-14 13:39:56 -0700

    Add Support for Plugins not in Update Center
    Add a way to include plugins that are not available through the Jenkins
    Update Center. This is done by providing an optional URL portion to the
    entries inside the plugins array. If the URL is present, the Update
    Center is skipped and the file downloaded directly from the URL.

--- scripts/plugin_install.sh
+++ scripts/plugin_install.sh
@@ -14,29 +14,30 @@ JENKINS_UPDATE_CENTER_JSON=$(curl -sL https://updates.jenkins-ci.org/update-cent
 
 # Jenkins plugins are specified here, following the format "pluginname/version"
 JENKINS_PLUGINS=(
-    "ansicolor/0.4.2"
-    "build-pipeline-plugin/1.4.9"
-    "credentials/1.24"
-    "credentials-binding/1.6"
-    "git/2.4.1"
-    "git-client/1.19.2"
-    "github/1.17.1"
+    "ansicolor~0.4.2"
+    "build-pipeline-plugin~1.4.9"
+    "credentials~1.24"
+    "credentials-binding~1.6"
+    "git~2.4.1"
+    "git-client~1.19.2"
+    "github~1.17.1"
     "github-api/1.72.1"
-    "greenballs/1.15"
-    "jquery/1.7.2-1"
-    "job-dsl/1.42"
-    "jobConfigHistory/2.12"
-    "mesos/0.11.0"
-    "monitoring/1.58.0"
-    "parameterized-trigger/2.30"
-    "plain-credentials/1.1"
-    "rebuild/1.25"
-    "saferestart/0.3"
-    "scm-api/0.2"
-    "script-security/1.17"
-    "ssh-credentials/1.11"
-    "token-macro/1.12.1"
-    "workflow-step-api/1.14"
+    "greenballs~1.15"
+    "jquery~1.7.2-1"
+    "job-dsl~1.42"
+    "jobConfigHistory~2.12"
+    "mesos~0.11.0"
+    "monitoring~1.58.0"
+    "parameterized-trigger~2.30"
+    "plain-credentials~1.1"
+    "rebuild~1.25"
+    "saferestart~0.3"
+    "scm-api~0.2"
+    "script-security~1.17"
+    "ssh-credentials~1.11"
+    "token-macro~1.12.1"
+    "workflow-step-api~1.14"
+    "marathon~1.1~https://s3.amazonaws.com/downloads.mesosphere.io/velocity/jenkins/plugins/marathon-1.1.hpi"
 )
 
 # Create $JENKINS_PLUGINS_DIR if it doesn't exist
@@ -44,11 +45,13 @@ if [[ ! -d "$JENKINS_PLUGINS_DIR" ]]; then
     mkdir -p "$JENKINS_PLUGINS_DIR"
 fi
 
+# Check the Update Center JSON for latest version of plugin
+# and compare against the configured version in JENKINS_PLUGINS
 function check_for_update {
     plugin_name=$1
     plugin_ver=$2
 
-    latest_plugin_ver=$(echo $JENKINS_UPDATE_CENTER_JSON | jq -r ".plugins | .[\"${plugin_name}\"] | .version")
+    local latest_plugin_ver=$(echo $JENKINS_UPDATE_CENTER_JSON | jq -r ".plugins | .[\"${plugin_name}\"] | .version")
     if [[ $plugin_ver == $latest_plugin_ver ]]; then
         echo "${plugin_name} is up to date."
     else
@@ -56,23 +59,46 @@ function check_for_update {
     fi
 }
 
-# Download each of the plugins specified in $JENKINS_PLUGINS
-for plugin in ${JENKINS_PLUGINS[@]}; do
-    IFS='/' read -a plugin_info <<< "${plugin}"
+# Set variables for the passed-in plugin, which is
+# an element from the JENKINS_PLUGINS array.
+function set_plugin_vars {
+    local plugin=$1
+    IFS='~' read -a plugin_info <<< "${plugin}"
     plugin_name=${plugin_info[0]}
     plugin_ver=${plugin_info[1]}
-
-    plugin_remote_path="${plugin_name}/${plugin_ver}/${plugin_name}.hpi"
+    plugin_url=${plugin_info[2]}
     plugin_local_path="${JENKINS_PLUGINS_DIR}/${plugin_name}.hpi"
+    plugin_remote_path="${plugin_name}/${plugin_ver}/${plugin_name}.hpi"
+}
 
-    echo "Downloading ${plugin_name} ${plugin_ver} ..."
-    check_for_update $plugin_name $plugin_ver
-    curl -fSL "${JENKINS_PLUGINS_MIRROR}/${plugin_remote_path}" -o $plugin_local_path 2> /dev/null
+# Download the hpi file from the given url.
+function fetch_plugin_hpi {
+    curl -fSL "${1}" -o $plugin_local_path 2> /dev/null
+}
 
+# Check the integrity of a downloaded zip file.
+function test_zip {
     # All Jenkins .hpi/.jpi files are actually Zip files. Let's check their
     # integrity during the build process so we dont have any surprises at
     # container run time.
-    zip --test $plugin_local_path
+    zip --test "${1}"
+}
+
+# Download each of the plugins specified in $JENKINS_PLUGINS
+for plugin in ${JENKINS_PLUGINS[@]}; do
+    set_plugin_vars $plugin
+
+    echo "Downloading ${plugin_name} ${plugin_ver} ..."
+
+    # if $plugin_url is null, then grab it from Update Center
+    # otherwise, download from the given url
+    if [[ -z $plugin_url ]]; then
+        check_for_update $plugin_name $plugin_ver
+        fetch_plugin_hpi "${JENKINS_PLUGINS_MIRROR}/${plugin_remote_path}"
+    else
+        fetch_plugin_hpi $plugin_url
+    fi
+    test_zip $plugin_local_path
 
     echo
 done

08fa0eb07f0144bcd278eb277989ec87242df61f
Colin C <colin@mesosphere.com>
2017-07-27 12:06:04 -0700

    Use printf instead of echo to preserve newlines

--- scripts/dcos-account.sh
+++ scripts/dcos-account.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 #
 # Write DC/OS credentials to file and update environment
 # variables accordingly.
@@ -11,13 +11,12 @@ write_cred_file()
 {
     if [ -n "$DCOS_SERVICE_ACCOUNT_CREDENTIAL" ] && [ "$DCOS_SERVICE_ACCOUNT_CREDENTIAL" != file* ]; then
         local target_file="$JENKINS_HOME/.dcos_acct_creds"
-        echo "$DCOS_SERVICE_ACCOUNT_CREDENTIAL" > $target_file
+        printf "%s" "$DCOS_SERVICE_ACCOUNT_CREDENTIAL" > $target_file
 
         # fix env variable
         DCOS_SERVICE_ACCOUNT_CREDENTIAL="file://$target_file"
         export DCOS_SERVICE_ACCOUNT_CREDENTIAL
     fi
 }
-
 
 write_cred_file

0b703295b5108b6f0a415347185e6c5e1674d213
benclarkwood <ben@benclarkwood.com>
2018-04-23 10:59:23 -0700

    Add additional TODO

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@
 docker build .
 
 # docker publish to user docker repo (e.g. DOCKER_REPO envvar)
-# TODO Colin
+# TODO Colin or Tarun
+
+# Set DOCKER_IMAGE envvar to value user supplied
+# TODO Colin or Tarun
 
 # Update universe/ or using tooling to reference the correct image (via templating)
 # TODO Tarun

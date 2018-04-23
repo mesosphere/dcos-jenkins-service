afb55257c1f44f076931a2850dcc6a65a26bccf0
benclarkwood <ben@benclarkwood.com>
2018-04-23 10:57:01 -0700

    Add TODOs

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# If DOCKER_IMAGE envvar set then use that
+# Else build DOCKER_IMAGE
+
+# docker build
+docker build .
+
+# docker publish to user docker repo (e.g. DOCKER_REPO envvar)
+# TODO Colin
+
+# Update universe/ or using tooling to reference the correct image (via templating)
+# TODO Tarun
+
+# Publish stub
+tools/publish_aws.py jenkins universe/

76b69a74797702aa6d31e4cb97e13cbf24f9a573
Tarun Gupta Akirala <takirala@users.noreply.github.com>
2018-04-25 11:16:28 -0700

    Add groovy scripts for jenkins remote access (#195)
    * Add groovy scripts for jenkins remote access
    
    * Better naming conventions
    
    * Delete jobs command

---
+++ testing/jenkins_remote_access.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+
+import logging
+from string import Template
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
+
+IMPORTS = """
+import org.jenkinsci.plugins.mesos.MesosCloud;
+import org.jenkinsci.plugins.mesos.MesosSlaveInfo;
+import org.apache.mesos.Protos;
+import hudson.slaves.NodeProperty;
+import jenkins.model.*
+"""
+
+DOCKER_CONTAINER = """
+def containerInfo = new MesosSlaveInfo.ContainerInfo(
+                "",
+                "mesosphere/jenkins-dind:0.5.0-alpine",
+                true,
+                false,
+                false,
+                true,
+                "wrapper.sh",
+                new LinkedList<MesosSlaveInfo.Volume>(),
+                new LinkedList<MesosSlaveInfo.Parameter>(),
+                Protos.ContainerInfo.DockerInfo.Network.BRIDGE.name(),
+                new LinkedList<MesosSlaveInfo.PortMapping>(),
+                new LinkedList<MesosSlaveInfo.NetworkInfo>()
+)
+"""
+
+MESOS_SLAVE_INFO_OBJECT = """
+def mesosSlaveInfo = new MesosSlaveInfo(
+        "$labelString",
+        $mode,
+        "$slaveCpus",
+        "$slaveMem",
+        "$minExecutors",
+        "$maxExecutors",
+        "$executorCpus",
+        "$diskNeeded",
+        "$executorMem",
+        "$remoteFSRoot",
+        "$idleTerminationMinutes",
+        "$slaveAttributes",
+        "$jvmArgs",
+        "$jnlpArgs",
+        "$defaultSlave",
+        $containerInfo,
+        new LinkedList<URI>(),
+        new LinkedList<? extends NodeProperty<?>>()
+)
+"""
+
+MESOS_SLAVE_INFO_ADD = """
+MesosCloud cloud = MesosCloud.get();
+cloud.getSlaveInfos().add(mesosSlaveInfo)
+cloud.getSlaveInfos().each {
+        t ->
+            println("Label : " + t.getLabelString())
+}
+"""
+
+MESOS_SLAVE_INFO_REMOVE = """
+MesosCloud cloud = MesosCloud.get();
+Iterator<MesosSlaveInfo> it = cloud.getSlaveInfos().iterator()
+while(it.hasNext()) {
+    MesosSlaveInfo msi = it.next();
+    if (msi.getLabelString().equals("$labelString")){
+        it.remove()
+    }
+}
+cloud.getSlaveInfos().each {
+        t ->
+            println("Label : " + t.getLabelString())
+}
+"""
+
+
+DELETE_JOBS = """
+def matchedJobs = Jenkins.instance.items.findAll { job ->
+    job.name =~ /$JOBS_REGEX/
+}
+    
+matchedJobs.each { job ->
+    println job.name
+    job.delete()
+}
+"""
+
+
+def add_slave_info(
+        labelString,
+        slaveCpus="0.5",
+        slaveMem="512",
+        minExecutors="1",
+        maxExecutors="1",
+        executorCpus="0.1",
+        diskNeeded="0.0",
+        executorMem="128",
+        mode="Node.Mode.NORMAL",
+        remoteFSRoot="/mnt/mesos/sandbox",
+        idleTerminationMinutes="3",
+        slaveAttributes="",
+        jvmArgs="-Xms16m -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true",
+        jnlpArgs="-noReconnect",
+        defaultSlave="false",
+):
+    slaveInfo = Template(MESOS_SLAVE_INFO_OBJECT).substitute({
+         "labelString": labelString,
+         "mode": mode,
+         "slaveCpus": slaveCpus,
+         "slaveMem": slaveMem,
+         "minExecutors": minExecutors,
+         "maxExecutors": maxExecutors,
+         "executorCpus": executorCpus,
+         "diskNeeded": diskNeeded,
+         "executorMem": executorMem,
+         "remoteFSRoot": remoteFSRoot,
+         "idleTerminationMinutes": idleTerminationMinutes,
+         "slaveAttributes": slaveAttributes,
+         "jvmArgs": jvmArgs,
+         "jnlpArgs": jnlpArgs,
+         "defaultSlave": defaultSlave,
+         "containerInfo": "containerInfo",
+    })
+    return make_post(
+        DOCKER_CONTAINER +
+        slaveInfo +
+        MESOS_SLAVE_INFO_ADD
+    )
+
+
+def remove_slave_info(labelString):
+    return make_post(
+        Template(MESOS_SLAVE_INFO_REMOVE).substitute(
+            {
+                "labelString": labelString
+            }
+        )
+    )
+
+
+def remove_all_jobs(regex):
+    return make_post(
+        Template(DELETE_JOBS).substitute(
+            {
+                'JOBS_REGEX': regex
+            }
+        )
+    )
+
+
+def make_post(
+        post_body,
+        service_name='jenkins'
+):
+    """
+    :rtype: requests.Response
+    """
+    body = IMPORTS + post_body
+    log.info('\nMaking request : ========\n{}\n========\n'.format(body))
+    '''
+    Note: To run locally:
+    curl -i -H "Authorization:token=$(dcos config show core.dcos_acs_token)" \
+         -k --data-urlencode "script=$(< <path-to-above-script-file>)" \
+         https://<dcos-cluster>/service/jenkins/scriptText'
+    '''
+    import sdk_cmd
+    return sdk_cmd.service_request(
+        'POST',
+        service_name,
+        'scriptText',
+        log_args=False,
+        data=body
+    )
+
+if __name__ == '__main__':
+    remove_all_jobs("(.*)")
--- tests/scale/test_scale_utils.py
+++ tests/scale/test_scale_utils.py
@@ -4,11 +4,14 @@ import uuid
 
 import config
 import jenkins
+import jenkins_remote_access
 import sdk_install
+import sdk_utils
 
 log = logging.getLogger(__name__)
 
 test_job_name = 'test-job-{}'.format(uuid.uuid4())
+
 
 @pytest.fixture(scope='module', autouse=True)
 def configure_package():
@@ -16,7 +19,7 @@ def configure_package():
         sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
         sdk_install.install(config.PACKAGE_NAME, config.SERVICE_NAME, 0, wait_for_deployment=False)
 
-        yield # let the test session execute
+        yield  # let the test session execute
     finally:
         sdk_install.uninstall(config.PACKAGE_NAME, config.SERVICE_NAME)
 
@@ -27,4 +30,12 @@ def test_create_job():
     job = jenkins.get_job(config.SERVICE_NAME, test_job_name)
 
     assert test_job_name == job['name']
-    
+
+
+@pytest.mark.sanity
+def test_jenkins_remote_access():
+    rand_label = sdk_utils.random_string()
+    r = jenkins_remote_access.add_slave_info(rand_label)
+    assert r.status_code == 200, 'Got {} when trying to post MesosSlaveInfo'.format(r.status_code)
+    r = jenkins_remote_access.remove_slave_info(rand_label)
+    assert r.status_code == 200, 'Got {} when trying to remove label'.format(r.status_code)

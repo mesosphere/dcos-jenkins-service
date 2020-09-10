758611e636870c1a4055c66e8815ea99c2fac5fb
Kaiwalya Joshi <kjoshi@mesosphere.com>
2020-09-10 16:10:17 -0700

    Fix early exit from dcos-quota.sh script.

--- scripts/dcos-quota.sh
+++ scripts/dcos-quota.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # This script enables multi-tenancy features available from DC/OS 2.0 and later. 
 # This script forces $JENKINS_AGENT_ROLE to match $MESOS_ALLOCATION_ROLE
 # Only when $MARATHON_APP_ENFORCE_GROUP_ROLE is set.
@@ -21,20 +20,19 @@ _str2bool() {
 
 if [ -z "$MESOS_ALLOCATION_ROLE" ] || [ -z "$MARATHON_APP_ENFORCE_GROUP_ROLE" ]; then
     echo "INFO: This cluster does not have multi-tenant features. Using legacy non-quota enforcement aware semantics."
-    exit 0
-fi
-
-#Filter all variants of true here.
-_str2bool $MARATHON_APP_ENFORCE_GROUP_ROLE
-
-if [ ! -z "$IS_ENFORCE_GROUP_ROLE" ]; then
-    if [ "$MESOS_ALLOCATION_ROLE" != "$JENKINS_AGENT_ROLE" ]; then
-        echo "WARN: JENKINS_AGENT_ROLE:'$JENKINS_AGENT_ROLE' not the same as MESOS_ALLOCATION_ROLE:'$MESOS_ALLOCATION_ROLE'." 
-        echo "enforceRole detected on top-level group, using '$MESOS_ALLOCATION_ROLE' as agent-role." 
-    fi 
-    JENKINS_AGENT_ROLE=$MESOS_ALLOCATION_ROLE 
-    export $JENKINS_AGENT_ROLE
-    echo "INFO: using enforced group role '$JENKINS_AGENT_ROLE' for agents."
 else
-    echo "INFO: using non-enforced group role '$JENKINS_AGENT_ROLE' for agents."
+    #Filter all variants of true here.
+    _str2bool $MARATHON_APP_ENFORCE_GROUP_ROLE
+
+    if [ ! -z "$IS_ENFORCE_GROUP_ROLE" ]; then
+        if [ "$MESOS_ALLOCATION_ROLE" != "$JENKINS_AGENT_ROLE" ]; then
+            echo "WARN: JENKINS_AGENT_ROLE:'$JENKINS_AGENT_ROLE' not the same as MESOS_ALLOCATION_ROLE:'$MESOS_ALLOCATION_ROLE'." 
+            echo "enforceRole detected on top-level group, using '$MESOS_ALLOCATION_ROLE' as agent-role." 
+        fi 
+        JENKINS_AGENT_ROLE=$MESOS_ALLOCATION_ROLE 
+        export $JENKINS_AGENT_ROLE
+        echo "INFO: using enforced group role '$JENKINS_AGENT_ROLE' for agents."
+    else
+        echo "INFO: using non-enforced group role '$JENKINS_AGENT_ROLE' for agents."
+    fi
 fi
